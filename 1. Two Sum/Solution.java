0c4f77025a743a4dd7174cce345ec5a740b776cc
xugang <chuangcius@gmail.com>
2022-06-08 17:51:15 +0800

    update: 1. Two Sum

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Solution
@@ -9,12 +11,13 @@ import java.util.Arrays;
 public class Solution {
 
     public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> numsMap = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int candidate = target - nums[i];
+            if (numsMap.containsKey(candidate)) {
+                return new int[]{i, numsMap.get(candidate)};
             }
+            numsMap.put(nums[i], i);
         }
         return new int[]{};
     }
