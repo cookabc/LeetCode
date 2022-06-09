4b39446df677613631f5cbf5e9af5afd7db444e9
xugang <chuangcius@gmail.com>
2022-06-09 21:41:51 +0800

    add: 14. Longest Common Prefix

---
+++ 14. Longest Common Prefix/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Solution
+ *
+ * @author xugang.song
+ * @date 2022.06.09
+ */
+public class Solution {
+
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        Arrays.sort(strs, Comparator.comparing(String::length));
+        String base = strs[0];
+        for (int i = 0; i < base.length(); i++) {
+            boolean appendable = true;
+            for (String str : strs) {
+                if (str.charAt(i) != base.charAt(i)) {
+                    appendable = false;
+                    break;
+                }
+            }
+            if (appendable) {
+                result.append(base.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+    }
+}
