1c46c90b99355bf0653bdee596c4a3853c6ea0f5
xugang <chuangcius@gmail.com>
2022-06-11 20:37:38 +0800

    add: 27. Remove Element

---
+++ 27. Remove Element/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Solution
+ *
+ * @author xugang.song
+ * @date 2022.06.11
+ */
+public class Solution {
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
+        System.out.println(removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
+    }
+}
