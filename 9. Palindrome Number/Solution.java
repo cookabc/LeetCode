cf87c97a596979595a88ae44b001b9fa84810f2c
xugang <chuangcius@gmail.com>
2022-06-11 08:48:28 +0800

    update2: 9. Palindrome Number

--- 9. Palindrome Number/Solution.java
+++ 9. Palindrome Number/Solution.java
@@ -7,18 +7,15 @@
 public class Solution {
 
     public static boolean isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-
-        int originalNum = x;
         int newNum = 0;
-        while (x != 0) {
-            int reminder = x % 10;
-            newNum = newNum * 10 + reminder;
+        while (x > newNum) {
+            newNum = newNum * 10 + x % 10;
             x = x / 10;
         }
-        return originalNum == newNum;
+        return x == newNum || x == newNum / 10;
     }
 
     public static void main(String[] args) {
