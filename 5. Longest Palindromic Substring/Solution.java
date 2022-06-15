cd92ed3d96a67603a735a9b6d9a387d28765afe0
xugang <chuangcius@gmail.com>
2022-06-15 10:25:35 +0800

    update: 5. Longest Palindromic Substring

--- 5. Longest Palindromic Substring/Solution.java
+++ 5. Longest Palindromic Substring/Solution.java
@@ -7,27 +7,31 @@
 public class Solution {
 
     public static String longestPalindrome(String s) {
-        if (s == null || s.length() < 2) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        if (s.length() == 1) {
             return s;
         }
-        for (int len = s.length(); len > 1; len--) {
-            for (int i = 0; i <= s.length() - len; i++) {
-                String candidate = s.substring(i, len + i);
-                if (isPalindrome(candidate)) {
-                    return candidate;
-                }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return String.valueOf(s.charAt(0));
+        return s.substring(start, end + 1);
     }
 
-    private static boolean isPalindrome(String s) {
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                return false;
-            }
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return true;
+        return right - left - 1;
     }
 
     public static void main(String[] args) {
