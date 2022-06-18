e6f857fe8bed49fcfb96c14524d55ce0151a59a4
xugang <chuangcius@gmail.com>
2022-06-18 09:36:37 +0800

    add: 1342. Number of Steps to Reduce a Number to Zero

---
+++ 1342. Number of Steps to Reduce a Number to Zero/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Solution
+ *
+ * @author xugang.song
+ * @date 2022.06.18
+ */
+public class Solution {
+
+    public static int numberOfSteps(int num) {
+        int count = 0;
+        while (num != 0) {
+            count++;
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(14));
+        System.out.println(numberOfSteps(8));
+        System.out.println(numberOfSteps(123));
+    }
+}
