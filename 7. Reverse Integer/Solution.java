9f661f1fe9f1c589c7a9cf6aa18769a3ad6fa42d
xugang <chuangcius@gmail.com>
2022-06-19 17:24:42 +0800

    add: 7. Reverse Integer

---
+++ 7. Reverse Integer/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Solution
+ *
+ * @author xugang.song
+ * @date 2022.06.19
+ */
+public class Solution {
+
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            result = result * 10 + pop;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(0));
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+        System.out.println(reverse(120));
+        System.out.println(reverse(1534236469));
+    }
+}
