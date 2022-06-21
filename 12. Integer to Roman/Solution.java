e17b37743910d8965a39de7fff235770ee1b393d
xugang <chuangcius@gmail.com>
2022-06-21 11:45:44 +0800

    update: 12. Integer to Roman

--- 12. Integer to Roman/Solution.java
+++ 12. Integer to Roman/Solution.java
@@ -13,46 +13,30 @@ public class Solution {
             num %= 1000;
         }
         if (num >= 100) {
-            int hCount = num / 100;
-            if (hCount == 9) {
-                sb.append("CM");
-            } else if (hCount > 4) {
-                sb.append("D");
-                sb.append("C".repeat(hCount - 5));
-            } else if (hCount == 4) {
-                sb.append("CD");
-            } else {
-                sb.append("C".repeat(hCount));
-            }
+            append(sb, "C", "M", "D",  num / 100);
             num %= 100;
         }
         if (num >= 10) {
-            int hCount = num / 10;
-            if (hCount == 9) {
-                sb.append("XC");
-            } else if (hCount > 4) {
-                sb.append("L");
-                sb.append("X".repeat(hCount - 5));
-            } else if (hCount == 4) {
-                sb.append("XL");
-            } else {
-                sb.append("X".repeat(hCount));
-            }
+            append(sb, "X", "C", "L",  num / 10);
             num %= 10;
         }
         if (num >= 0) {
-            if (num == 9) {
-                sb.append("IX");
-            } else if (num > 4) {
-                sb.append("V");
-                sb.append("I".repeat(num - 5));
-            } else if (num == 4) {
-                sb.append("IV");
-            } else {
-                sb.append("I".repeat(num));
-            }
+            append(sb, "I", "X", "V", num);
         }
         return sb.toString();
+    }
+
+    private static void append(StringBuilder sb, String base, String upper, String lower, int count) {
+        if (count == 9) {
+            sb.append(base).append(upper);
+        } else if (count > 4) {
+            sb.append(lower);
+            sb.append(base.repeat(count - 5));
+        } else if (count == 4) {
+            sb.append(base).append(lower);
+        } else {
+            sb.append(base.repeat(count));
+        }
     }
 
     public static void main(String[] args) {
