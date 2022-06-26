ed4949df674ace3b681aacdbaa61a5290f4b23ee
xugang <chuangcius@gmail.com>
2022-06-27 02:22:07 +0800

    update: 34. Find First and Last Position of Element in Sorted Array

--- 34. Find First and Last Position of Element in Sorted Array/Solution.java
+++ 34. Find First and Last Position of Element in Sorted Array/Solution.java
@@ -9,21 +9,37 @@ import java.util.Arrays;
 public class Solution {
 
     public static int[] searchRange(int[] nums, int target) {
-        int start = -1, end = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                if (start == -1) {
-                    start = i;
-                }
-                end = i;
-            }
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
         }
-        return new int[]{start, end};
+        int start, end;
+        int mid = nums.length / 2;
+        if (nums[mid] > target) {
+            return searchRange(Arrays.copyOfRange(nums, 0, mid), target);
+        } else if (nums[mid] < target) {
+            int[] range = searchRange(Arrays.copyOfRange(nums, mid + 1, nums.length), target);
+            if (range[0] == -1) {
+                return new int[]{-1, -1};
+            } else {
+                return new int[]{mid + range[0] + 1, mid + range[1] + 1};
+            }
+        } else {
+            start = mid;
+            end = mid;
+            while (start > 0 && nums[start - 1] == target) {
+                start--;
+            }
+            while (end < nums.length - 1 && nums[end + 1] == target) {
+                end++;
+            }
+            return new int[]{start, end};
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
-        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
-        System.out.println(Arrays.toString(searchRange(new int[]{}, 0)));
+        System.out.println(Arrays.toString(searchRange(new int[]{1, 2, 3}, 3)));
+//        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+//        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
+//        System.out.println(Arrays.toString(searchRange(new int[]{}, 0)));
     }
 }
