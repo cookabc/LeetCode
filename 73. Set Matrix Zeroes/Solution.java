0c2154434859315718242c138bde92b9ef67905f
xugang <chuangcius@gmail.com>
2022-07-03 11:29:12 +0800

    update: 73. Set Matrix Zeroes

--- 73. Set Matrix Zeroes/Solution.java
+++ 73. Set Matrix Zeroes/Solution.java
@@ -9,28 +9,35 @@ public class Solution {
     public static void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[] rows = new int[m];
-        int[] cols = new int[n];
+        int fistZero = matrix[0][0];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    rows[i] = 1;
-                    cols[j] = 1;
-                }
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            if (rows[i] == 1) {
-                for (int j = 0; j < n; j++) {
-                    matrix[i][j] = 0;
-                }
-            } else {
-                for (int j = 0; j < n; j++) {
-                    if (cols[j] == 1) {
-                        matrix[i][j] = 0;
+                    if (i == 0) {
+                        fistZero = 0;
+                    } else {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
                     }
                 }
             }
+        }
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (fistZero == 0) {
+            matrix[0] = new int[n];
         }
     }
 
@@ -50,5 +57,9 @@ public class Solution {
         printMatrix(matrix);
         int[][] matrix2 = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
         printMatrix(matrix2);
+        int[][] matrix3 = {{1, 2, 3, 4}, {5, 0, 7, 8}, {0, 10, 11, 12}, {13, 14, 15, 0}};
+        printMatrix(matrix3);
+        int[][] matrix4 = {{1, 0, 3}};
+        printMatrix(matrix4);
     }
 }
