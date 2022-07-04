a14ff83fc09d9f4e43a55300634338f28fb2be69
xugang <chuangcius@gmail.com>
2022-07-04 08:43:26 +0800

    update: 69. Sqrt(x)

--- 69. Sqrt(x)/Solution.java
+++ 69. Sqrt(x)/Solution.java
@@ -10,17 +10,18 @@ public class Solution {
         if (x == 0) {
             return 0;
         }
-        if (x < 4) {
-            return 1;
+        int left = 1, right = Integer.MAX_VALUE;
+        while (true) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) {
+                right = mid - 1;
+            } else {
+                if (mid + 1 > x / (mid + 1)) {
+                    return mid;
+                }
+                left = mid + 1;
+            }
         }
-        long s = x / 2;
-        while (s * s > x) {
-            s /= 2;
-        }
-        while (s * s <= x) {
-            s += 1;
-        }
-        return (int) s - 1;
     }
 
     public static void main(String[] args) {
