4cb050b60b26284f2bca8a38197710d35d70f0c7
xugang <chuangcius@gmail.com>
2022-07-06 09:58:35 +0800

    add: 79. Word Search

---
+++ 79. Word Search/Solution.java
@@ -0,0 +1,57 @@
+/**
+ * Solution
+ *
+ * @author xugang.song
+ * @date 2022.07.06
+ */
+public class Solution {
+
+    public static boolean exist(char[][] board, String word) {
+        for (int m = 0; m < board.length; m++) {
+            for (int n = 0; n < board[0].length; n++) {
+                if (exist(board, word, m, n, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean exist(char[][] board, String word, int i, int j, int k) {
+        if (k == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return false;
+        }
+        if (board[i][j] != word.charAt(k)) {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#';
+        boolean res = exist(board, word, i + 1, j, k + 1)
+                || exist(board, word, i - 1, j, k + 1)
+                || exist(board, word, i, j + 1, k + 1)
+                || exist(board, word, i, j - 1, k + 1);
+        board[i][j] = temp;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(exist(new char[][]{
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
+        }, "ABCCED"));
+        System.out.println(exist(new char[][]{
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
+        }, "SEE"));
+        System.out.println(exist(new char[][]{
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
+        }, "ABCB"));
+    }
+}
